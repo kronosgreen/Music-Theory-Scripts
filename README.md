91bc0e55d3c631872f85465515fa42aac7adcfe0
Christopher Medrano <703137326@uniparks.com>
2024-03-06 00:30:33 -0500

    fixed up documentation

--- README.md
+++ README.md
@@ -4,4 +4,10 @@ Some command line scripts to get interesting music theory information.
 
 ## Harris Permutations
 
-Takes two 7th chords and returns all possible combinations between the two.
+Takes two four note chords and returns all possible combinations between the two.
+
+Run the script by passing in two chord names as arguments e.g.
+
+```
+python harris_permutations.py C7 Adim6
+```
--- harris_permutations.py
+++ harris_permutations.py
@@ -3,9 +3,10 @@ import musicpy as music
 import itertools
 
 
-parser = argparse.ArgumentParser(description='Harris Permutations')
+parser = argparse.ArgumentParser(description="Following Barry Harris's teachings, takes two four note chords " + \
+                                 "and finds all possible 4 note chord combinations of the combined notes.")
 
-parser.add_argument('chord_1', type=str, help='First chord')
+parser.add_argument('chord_1', type=str, help='First chord e.g. Cmaj7, Dm7, etc.')
 parser.add_argument('chord_2', type=str, help='Second chord')
 
 args = parser.parse_args()
@@ -16,12 +17,10 @@ chord_2 = music.C(args.chord_2)
 notes_1 = chord_1.notes
 notes_2 = chord_2.notes
 
-print(type(notes_1[0]).note)
-
 print(f"First Chord: {args.chord_1}: {notes_1}")
-print(f"First Chord: {args.chord_2}: {notes_2}")
+print(f"Second Chord: {args.chord_2}: {notes_2}")
 
-print(f"Permutations:")
+print("\nPermutations:")
 for first_chord in itertools.combinations(notes_1 + notes_2, r=4):
     second_chord = [x for x in notes_1 + notes_2 if x not in first_chord]
     print(f"{list(first_chord)}, {second_chord}")
